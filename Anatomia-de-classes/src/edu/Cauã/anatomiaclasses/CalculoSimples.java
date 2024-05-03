91b29acebfb4237c217bc11c1c4a2016b25b454d
Kant_xlx <nada862979@gmail.com>
2024-05-03 16:08:34 -0300

    Subindo anatomia das classe e o inicio sobre tipos e variaveis

---
+++ Anatomia-de-classes/src/edu/Cauã/anatomiaclasses/CalculoSimples.java
@@ -0,0 +1,13 @@
+package edu.Cauã.anatomiaclasses;
+public class CalculoSimples {
+    
+    public static void main(String[] args) {
+
+        int somar;
+        int numeroDois = 6;
+        int  numeroUm = 4;
+        somar = numeroUm + numeroDois;
+        System.out.println(somar);
+    }
+
+}
